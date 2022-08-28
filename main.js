0b82c492e97f9370d8b5167066e73255e9205821
Ivan <herrera.ammores@gmail.com>
2022-08-27 22:57:19 -0300

    ajuste

--- main.js
+++ main.js
@@ -1,17 +1,69 @@
+const fs = require('fs')
+
+class contenedor{
+
+    constructor(archivo){
+        this.archivo = archivo;
+
+    }
+    async save(objeto){
+        const data = await fs.promises.readFile(`${this.archivo}/producto.txt`, "utf8");
+        const producto = JSON.parse(data);
+        const id = producto.length +1;
+        objeto.id = id;
+        producto.push(objeto);
+        console.log(objeto.id);
+        const productoString = JSON.stringify(producto);
+        await fs.promises.writeFile(`${this.archivo}/producto.txt`, productoString);
+        
+
+    }
+    async getById(id){
+        const data = await fs.promises.readFile(`${this.archivo}/producto.txt`, 'utf8');
+        const productos = JSON.parse(data);
+        const producto = productos.find((producto) => producto.id === id);
+        if (producto) {
+            return producto;
+        }else{
+            return null;
+        }
+
+    }
+    async getAll(){
+        const data = await fs.promises.readFile(`${this.archivo}/producto.txt`, 'utf8');
+        return JSON.parse(data);
+    }
+    async deleteById(id){
+        const data = await fs.promises.readFile(`${this.archivo}/producto.txt`, 'utf8');
+        const productos = JSON.parse(data);
+        const producto = productos.find((producto) => producto.id === id);
+        let index = productos.indexOf(producto);
+        productos.splice(index, 1);
+        const productoString = JSON.stringify(productos);
+        await fs.promises.writeFile(`${this.archivo}/producto.txt`, productoString);
+
+    }
+    async deleteAll(){
+        const productos = [];
+        const productoString = JSON.stringify(productos);
+        await fs.promises.writeFile(`${this.archivo}/producto.txt`, productoString);
+    }
+}
+
+
+
 const express = require('express');
 const app = express();
 
 app.get('/productos',(req, res) =>{
-  res.sendFile(`./data/producto.json`, {
+  res.sendFile(`./data/producto.txt`, {
     root: __dirname
   })
 });
 
 
-let contador = 0;
 app.get('/productosRandom', function(req, res) {
-  contador++;
-  res.send(`<h1>La cantidad de visitas es ${contador}</h1>`);
+  res.send(`<h1>Producto random/h1>`);
 });
 
 
