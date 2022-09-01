a3123a8c331618eebaaf15fb81e34ed78862389e
Ivan <herrera.ammores@gmail.com>
2022-08-31 21:42:42 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const express = require("express");
-
+const express = require('express');
+const db = require('./db.js');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const DB = new db('data')
+
+
+
+
+app.get('/productos',async (req, res) =>{
+  const data = await DB.getAll();
+  res.send(data)
+});
+
+app.get('/productosRandom',async (req, res) =>{
+  let number = await DB.getAll()
+  let id =parseInt(Math.floor(Math.random() * number.length+1));
+  const data = await DB.getById(id);
+  res.send(data)
+});
+
+
+app.listen(8080, ()=>{
+  console.log('server iniciado'); 
+})
+ 
